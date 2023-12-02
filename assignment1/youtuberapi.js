105422021ab3b496fda15424c648b90d095a2193
okty1227 <okty12271227@icloud.com>
2023-12-01 21:06:51 -0500

    clean project1

--- assignment1/youtuberapi.js
+++
@@ -1,6 +0,0 @@
-const SocialBlade = require("socialblade");
-
-const client = new SocialBlade(SOCIALBLADE_CLIENT_ID, SOCIALBLADE_ACCESS_TOKEN);
-
-// Get a YouTube User
-client.youtube.user('socialblade').then(console.log);
